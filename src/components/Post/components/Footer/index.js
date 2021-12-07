10977c2e3469d569d838b3c7de9ba2442a60bd7a
qazqaz1000 <qazqaz1000@hanmail.net>
2021-12-07 23:20:52 +0900

    like 추가

--- src/components/Post/components/Footer/index.js
+++ src/components/Post/components/Footer/index.js
@@ -1,27 +1,52 @@
-import React from "react";
-import {View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import {View, Text, TouchableWithoutFeedback} from "react-native";
 
 import FAIcon from 'react-native-vector-icons/FontAwesome';
 import ADIcon from 'react-native-vector-icons/AntDesign';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 
 import styles from "./styles";
-const Footer = ({likesCount, caption, postedAt}) => (
-    <View style={styles.container}>
-        <View style={styles.iconsContainer}>
-            <View style={styles.leftIcons}>
-                <ADIcon name="hearto" size={25} color={"#545454"}></ADIcon>
-                <IonIcon name="chatbubble-outline" size={25} color={"#545454"}></IonIcon>
-                <IonIcon name="paper-plane-outline" size={25} color={"#545454"}></IonIcon>
+const Footer = ({likesCount : likesCountProp, caption, postedAt}) => {
+
+    const [isLiked, setIsLiked] = useState(false);
+    const [likesCount, setLikesCount] = useState(0);
+
+    const onLikePressed = () =>{
+        const amount = isLiked ? -1 : 1;
+        setLikesCount(likesCount + amount);
+        setIsLiked(!isLiked);
+    }
+
+
+    useEffect(()=>{
+        setLikesCount(likesCountProp);
+    }, [])
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.iconsContainer}>
+                <View style={styles.leftIcons}>
+                    <TouchableWithoutFeedback onPress={onLikePressed}>
+                        {isLiked ?
+                            <ADIcon name="heart" size={25} color={"#c30000"}></ADIcon>
+                            : <ADIcon name="hearto" size={25} color={"#545454"}></ADIcon>
+                        }
+                        
+                    </TouchableWithoutFeedback>
+                        
+                    
+                    <IonIcon name="chatbubble-outline" size={25} color={"#545454"}></IonIcon>
+                    <IonIcon name="paper-plane-outline" size={25} color={"#545454"}></IonIcon>
+                </View>
+
+                <FAIcon name="bookmark-o" size={25} color={"#545454"}></FAIcon>
             </View>
+            <Text style={styles.likes}>{likesCount} 좋아요</Text>
 
-            <FAIcon name="bookmark-o" size={25} color={"#545454"}></FAIcon>
+            <Text style={styles.caption}>{caption}</Text>
+            <Text style={styles.postedAt}>{postedAt}</Text>
         </View>
-        <Text style={styles.likes}>{likesCount} 좋아요</Text>
-
-        <Text style={styles.caption}>{caption}</Text>
-        <Text style={styles.postedAt}>{postedAt}</Text>
-    </View>
-)
+    )
+}
 
 export default Footer;
