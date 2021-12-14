3c4b365d67543099e15f34dcf407c68c46a2d393
qazqaz1000 <qazqaz1000@hanmail.net>
2021-12-15 07:25:50 +0900

    android 확인 스토리 넘기기

--- src/screens/StoryScreen/index.js
+++ src/screens/StoryScreen/index.js
@@ -4,24 +4,31 @@ import { Text ,
      ImageBackground,
     ActivityIndicator,
     TouchableWithoutFeedback,
-    Dimensions
+    Dimensions,
+    TextInput
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import styles from './styles'; 
 import storiesData from '../../data/stories';
+import ProfilePicture from '../../components/ProfilePicture';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// import { TextInput } from 'react-native-gesture-handler';
 
 const StoryScreen = () => {
     const [userStories, setUserStories] = useState(null);
     const [activeStoryIndex, setActiveStoryIndex] = useState(null);
     // const [activeStory, setActiveStory] = useState(null);
     const route = useRoute();
-
+    const navigation = useNavigation();
+    const userId = route.params.userId;
     
+    console.log(userId);
 
     useEffect(() => {
-        const userId = route.params.userId;
+
         const userStories = storiesData.find(storyData => storyData.user.id === userId);
         console.log("default");
         console.log(userStories);
@@ -29,10 +36,19 @@ const StoryScreen = () => {
         setActiveStoryIndex(0);
     }, []);
 
+    const navigateToNextUser = () =>{
+        navigation.push("Story", {userId : (parseInt(userId) + 1).toString() });
+    }
+
+    const navigateToPrevUser = () =>{
+        navigation.push("Story", {userId : (parseInt(userId) - 1).toString() });
+    }
     
     const handleNextStory = () =>{
         console.log("next");
         if(activeStoryIndex >= userStories.stories.length - 1){
+            console.log("next2");
+            navigateToNextUser();
             return;
         }
         setActiveStoryIndex(activeStoryIndex + 1);
@@ -41,6 +57,7 @@ const StoryScreen = () => {
     const handlePrevStory = () =>{
         console.log("prev");
         if(activeStoryIndex <= 0 ){
+            navigateToPrevUser();
             return;
         }
         setActiveStoryIndex(activeStoryIndex - 1);
@@ -73,7 +90,33 @@ const StoryScreen = () => {
     return(
     <SafeAreaView style={styles.container}>
         <TouchableWithoutFeedback onPress={handlePress}>
-            <ImageBackground source = {activeStory.imageUri} style={styles.image}></ImageBackground>
+            <ImageBackground source = {activeStory.imageUri} style={styles.image}>
+                
+                <View style={styles.userinfo}>
+                    <ProfilePicture uri={userStories.user.imageUri} size={50}></ProfilePicture>    
+                    <Text style={styles.userName}>{userStories.user.name}</Text>
+                    <Text style={styles.postedTime}>{activeStory.postedTime}</Text>
+                </View>
+                <View style={styles.bottomContainer}>
+                    <View style={styles.cameraButton}>
+                        <Feather name="camera" size={25} color={'#ffffff'}></Feather>
+                    </View>
+                    <View style={styles.textInputContainer}>
+                        <TextInput 
+                        editable
+                        placeholder="Send Message" 
+                        placeholderTextColor={"white"}
+                        style={styles.textInput}
+                        ></TextInput>
+                    </View>
+
+                    <View style={styles.messageButton}>
+                        <Ionicons name="paper-plane-outline" size={25} color={'#ffffff'}></Ionicons>
+                    </View>
+                    
+
+                </View>
+            </ImageBackground>
         </TouchableWithoutFeedback>
     </SafeAreaView>
     )
--- src/screens/StoryScreen/styles.js
+++ src/screens/StoryScreen/styles.js
@@ -8,9 +8,57 @@ const styles = StyleSheet.create({
     image:{
         flex:1,
         resizeMode : "cover",
-        justifyContent : "center",
-        width:"100%",
+        justifyContent : "space-between",
+        // width:"100%",
     },
+    userinfo : {
+        flexDirection : "row",
+        alignItems : "center",
+        marginTop : 10
+    },
+    userName : {
+        color : "white",
+        fontWeight : "500",
+        fontSize : 18,
+    },
+    postedTime : {
+        color : "#808080",
+        fontWeight : "500",
+        fontSize : 14,
+        marginLeft : 10
+    },
+    bottomContainer : {
+        flexDirection: "row",
+        alignItems : "center",
+        marginBottom: 10,
+    },
+    cameraButton : {
+        width : 50,
+        alignItems : "center",
+        justifyContent : "center"
+    },
+
+    messageButton : {
+        width : 50,
+        alignItems : "center",
+        justifyContent : "center"
+    },
+    textInput : {
+        height : "100%",
+        color : "white",
+        fontSize : 16
+    },
+
+    textInputContainer : {
+        flex:1,
+        height: 50,
+        borderWidth: 2,
+        borderRadius :25,
+        borderColor : "white",
+        // marginHorizontal : 10,
+        paddingHorizontal : 10,
+        // padding: 10,
+    }
 });
 
 export default styles;
